644f43952ca8c1283c3394f4efb9a4802347b68a
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-07-13 06:17:27 -0400

    feat: adding serializer gem

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::MerchantsController < ApplicationController
     def index
         render json: Merchant.all
     end
+
+    def show  
+        render json: Merchant.find(params[:id])
+    end
+
+    def create
+        render json: Merchant.create
+    end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,46 @@ describe "Merchants API" do
             expect(merchant[:name]).to be_a String
         end
     end
+
+    it "can get one merchant by its id" do
+        id = create(:merchant).id
+
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to eq(id)
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a String
+    end
+
+    it "can get all items for a given merchant id" do
+        id = create(:merchant).id
+
+        get "/api/v1/merchants/#{id}/items"
+
+        items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(items).to be_a Hash
+        expect(items).to have_key[:data]
+    end
+
+    # it "can create a new merchant" do
+    #     merchant_params = ({
+    #                                         name: 'Terrible Furniture Is Us'
+    #                                     })
+    #     headers = {"CONTENT_TYPE" => "application/json"}
+
+    #     post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
+    #     created_merchant = Merchant.last
+
+    #     expect(response).to be_successful
+    #     expect(created_merchant.name).to eq(merchant_params[:name])
+    # end
 end
