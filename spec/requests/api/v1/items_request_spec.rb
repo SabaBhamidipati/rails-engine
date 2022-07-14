5102c91a4dbe2b915e16d7a4b74bf68475434e88
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-07-14 16:44:22 -0400

    test: gets one item happy path

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -28,9 +28,26 @@ describe "Items API" do
     expect(items[:data].first[:attributes][:merchant_id]).to be_a Integer
   end
 
-  # it "gets one item" do
-  #   merchants = create_
-  # end
+  it "gets one item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant: merchant).id
+
+    get "/api/v1/items/#{id}"
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(item).to be_a Hash
+    expect(item).to have_key(:data)
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data]).to have_key(:attributes)
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+     expect(item[:data][:attributes][:unit_price]).to be_a Float
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a Integer
+  end
 end
 
    
